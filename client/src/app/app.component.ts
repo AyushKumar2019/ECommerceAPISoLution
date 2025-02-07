ea66d314a05316521deba800f45914eb0bc1103d
ayush kumar <ayush.kum2810@gmail.com>
2025-02-06 22:52:52 -0500

    Linked to API and added some front end changes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component , inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/Pagination';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  baseUrl = 'https://localhost:7010/api/';
+  private http = inject(HttpClient);
   title = 'client';
+  products:Product[]=[];
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(`${this.baseUrl}`+ 'products').subscribe({
+      next: data =>{ this.products = data.data;  console.log( data)},
+      error: err => console.error(err),
+      complete: () => console.log('done')
+    });
+
+  }
 }
